3f1dec1608712181de2ec2a11dd6940905a07882
yuelianglune <yuelianglune@163.com>
2025-10-15 21:53:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def main():
     others = 0
     
     for char in filtered_string:
-        if 'a' <= char <= 'z' or 'A' <= char <= 'Z':
+        if ('a' <= char <= 'z') or ('A' <= char <= 'Z'):
             letters += 1
-        elif '0' <= char <= '9':
+        elif ('0' <= char <= '9'):
             digits += 1
-        elif char == ' ':
+        elif (char == ' '):
             spaces += 1
         else:
             others += 1
